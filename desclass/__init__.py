0e2e72d8b4c4d8adfab73e1641ed4185808de177
Erin Sheldon <erin.sheldon@gmail.com>
2020-09-15 09:25:19 -0400

    import modules

--- desclass/__init__.py
+++ desclass/__init__.py
@@ -1,1 +1,16 @@
+# flake8: noqa
+
 __version__ = '0.9.0'
+
+from . import bf
+from . import conc
+from . import constants
+from . import fitting
+from . import flags
+from . import inject
+from . import outputs
+
+from . import priors
+from . import psf_flux
+from . import staramp
+from . import util
