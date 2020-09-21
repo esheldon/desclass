e88a0ff006a93929848113f7f627bb6aaf7f2189
Erin Sheldon <erin.sheldon@gmail.com>
2020-09-21 14:44:43 -0400

    code to calculate probabilities

--- desclass/fit_conc_pdf.py
+++ desclass/fit_conc_pdf.py
@@ -10,6 +10,7 @@ from numba import njit
 from . import staramp
 # from . import galamp
 from . import cem
+from .cem import gauss_set, gauss_eval_scalar
 
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -677,7 +678,7 @@ def interp_gp_old(xin, yin, xinterp):
     return y_pred, sigma
 
 
-def interp_gp(x, y, yerr, xinterp):
+def get_gp(x, y, yerr):
     from sklearn import gaussian_process
     from sklearn.gaussian_process.kernels import (
         Matern, WhiteKernel,
@@ -701,12 +702,89 @@ def interp_gp(x, y, yerr, xinterp):
     X = x.reshape(-1, 1)
     gp.fit(X, y)
 
+    return gp
+
+
+def interp_gp(x, y, yerr, xinterp):
+
+    gp = get_gp(x, y, yerr)
+
     y_pred, sigma = gp.predict(
         xinterp.reshape(-1, 1),
         return_std=True,
     )
 
     return y_pred, sigma
+
+
+@njit
+def sum_interpolated_gauss(nums, means, sigmas, conc, vals, gauss):
+
+    for i in range(nums.size):
+        gauss_set(
+            gauss,
+            nums[i],
+            means[i],
+            sigmas[i],
+        )
+        vals[i] += gauss_eval_scalar(gauss, conc[i])
+
+
+def calculate_prob(data, rmag, conc):
+
+    rmag = np.array(rmag, dtype='f8', copy=False)
+    conc = np.array(conc, dtype='f8', copy=False)
+
+    gmixes = data['gmix']
+    ngauss = gmixes['mean'].shape[1]
+
+    centers = data['rmag']
+
+    gal_sums = np.zeros(rmag.size)
+    star_sums = np.zeros(rmag.size)
+
+    gauss = np.zeros(1, dtype=cem.GAUSS_DTYPE)[0]
+
+    for igauss in range(ngauss):
+        nums = gmixes['num'][:, igauss]
+        means = gmixes['mean'][:, igauss]
+        sigmas = gmixes['sigma'][:, igauss]
+
+        ystd = (smooth_data_hann3(nums) - nums).std()
+        nums_interp, _ = interp_gp(centers, nums, ystd, rmag)
+
+        ystd = (smooth_data_hann3(means) - means).std()
+        means_interp, _ = interp_gp(centers, means, ystd, rmag)
+
+        if igauss < 2:
+            ysend = sigmas
+        else:
+            ysend = smooth_data_hann3(sigmas)
+
+        ystd = (smooth_data_hann3(sigmas) - sigmas).std()
+        sigmas_interp, _ = interp_gp(centers, ysend, ystd, rmag)
+
+        if igauss < 2:
+            sum_interpolated_gauss(
+                nums_interp, means_interp, sigmas_interp, conc, star_sums,
+                gauss,
+            )
+        else:
+            sum_interpolated_gauss(
+                nums_interp, means_interp, sigmas_interp, conc, gal_sums,
+                gauss,
+            )
+
+    tot_sums = gal_sums + star_sums
+
+    w, = np.where(tot_sums > 0)
+    prob_gal = np.zeros(rmag.size)
+    prob_star = np.zeros(rmag.size)
+
+    prob_gal[w] = gal_sums[w]/tot_sums[w]
+    prob_star[w] = star_sums[w]/tot_sums[w]
+
+    return prob_gal, prob_star
 
 
 def plot_purity(data, type, rng):
